96ada06841c60f08cf5d2a8c41e125becf10bcda
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2025-05-20 22:04:42 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-sky-800 text-white py-6">
       <div className="container mx-auto px-4 text-center">
-        <p>© {new Date().getFullYear()} Legend File Utility Tools. Powerd by Legendbyte.com.</p>
+        <p>© {new Date().getFullYear()} Legend Utility Tools. Powerd by Legendbyte.com.</p>
         {/* <div className="mt-2">
           <a href="/" className="hover:underline mx-2">Privacy Policy</a>
           <a href="/terms" className="hover:underline mx-2">Terms of Service</a>
